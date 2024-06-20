438239ec787c6e1d0d101520510aaa46f8f3fbb6
Wellyngton Molinari <wellymolinari97@gmail.com>
2024-06-19 23:08:21 -0300

    update

--- scripts.js
+++ scripts.js
@@ -38,4 +38,35 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
+
+    const sections = document.querySelectorAll('.container h2, .container h3'); // Seções que serão monitoradas
+    const navLinks = document.querySelectorAll('.sidebar a'); // Links na sidebar
+
+    // Função para atualizar o estado dos links da sidebar
+    function updateSidebarLinks() {
+        let currentSection = '1';
+
+        // Encontrar a seção atualmente visível
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 60; // Ajuste de offset para considerar o topo da tela
+            if (window.scrollY >= sectionTop) {
+                currentSection = section.getAttribute('id');
+            }
+        });
+
+        // Remover a classe 'active' de todos os links
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href').includes(currentSection)) {
+                link.classList.add('active');
+            }
+        });
+    }
+
+    // Atualizar links da sidebar ao carregar a página
+    updateSidebarLinks();
+
+    // Atualizar links da sidebar ao rolar a página
+    window.addEventListener('scroll', updateSidebarLinks);
 });
+
